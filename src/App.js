047e27b7c7fc305270895a718378e292c919693d
Steven Kwan <stevent.kwan@gmail.com>
2023-03-07 21:10:15 -0800

    minimum viable tic tac toe game completed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import Square from '../src/components/Square';
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    const marker = xIsNext ? 'X' : 'O';
+    nextSquares[i] = marker;
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`
+  } else {
+    status = "Next player: " + (xIsNext ? 'X' : 'O');
   }
 
   return (
     <React.Fragment>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -29,4 +42,24 @@ export default function Board() {
       </div>
     </React.Fragment>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i += 1) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
